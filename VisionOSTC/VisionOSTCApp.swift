257132c1126490061164275a293acaad045570c5
Sugiy <yugos.0322.dev@gmail.com>
2025-11-16 12:37:17 +0900

    Initial Commit

---
+++ VisionOSTC/VisionOSTCApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSTCApp.swift
+//  VisionOSTC
+//
+//  Created by Yugo Sugiyama on 2025/11/16.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSTCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionOSTCTests/VisionOSTCTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSTCTests.swift
+//  VisionOSTCTests
+//
+//  Created by Yugo Sugiyama on 2025/11/16.
+//
+
+import Testing
+@testable import VisionOSTC
+
+struct VisionOSTCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
